d4b339bd2b0823184ff81a60aa679f2e96344626
wassim <wassimrached404@gmail.com>
2023-12-14 23:45:09 +0100

    Add dummy data to Book table

--- projects/library-system/src/functions/insert_functions.sql
+++ projects/library-system/src/functions/insert_functions.sql
@@ -48,6 +48,35 @@ BEGIN
 		(get_random_non_author_person_id(), 'A thriller writer known for stories with unexpected twists and turns.');
 END$$;
 
+
+-- fill the 'Book' table with dummy data
+CREATE OR REPLACE PROCEDURE populate_dummy_books()
+LANGUAGE plpgsql AS $$
+BEGIN
+	INSERT INTO Book (isbn, title, author_id, publication_year, genre, total_pages, language)
+	VALUES
+	('9781234567890', 'Sample Book 1', get_random_author_id(), 2022, 'Fiction', 300, 'English'),
+	('9782345678901', 'Sample Book 2', get_random_author_id(), 2020, 'Mystery', 250, 'Spanish'),
+	('9783456789012', 'Sample Book 3', get_random_author_id(), 2019, 'Science Fiction', 400, 'French'),
+	('9784567890123', 'Sample Book 4', get_random_author_id(), 2021, 'Romance', 350, 'German'),
+	('9785678901234', 'Sample Book 5', get_random_author_id(), 2018, 'Thriller', 320, 'Italian'),
+	('9786789012345', 'Sample Book 6', get_random_author_id(), 2023, 'Fantasy', 280, 'Portuguese'),
+	('9787890123456', 'Sample Book 7', get_random_author_id(), 2017, 'Historical Fiction', 420, 'Russian'),
+	('9788901234567', 'Sample Book 8', get_random_author_id(), 2020, 'Biography', 300, 'Chinese'),
+	('9789012345678', 'Sample Book 9', get_random_author_id(), 2019, 'Comedy', 350, 'Japanese'),
+	('9780123456789', 'Sample Book 10', get_random_author_id(), 2021, 'Drama', 320, 'Arabic'),
+	('9781234567890', 'Sample Book 11', get_random_author_id(), 2018, 'Horror', 280, 'Korean'),
+	('9782345678901', 'Sample Book 12', get_random_author_id(), 2022, 'Action', 400, 'Hindi'),
+	('9783456789012', 'Sample Book 13', get_random_author_id(), 2020, 'Adventure', 300, 'Bengali'),
+	('9784567890123', 'Sample Book 14', get_random_author_id(), 2023, 'Poetry', 250, 'Turkish'),
+	('9785678901234', 'Sample Book 15', get_random_author_id(), 2017, 'Self-Help', 420, 'Dutch'),
+	('9786789012345', 'Sample Book 16', get_random_author_id(), 2021, 'Science', 350, 'Swedish'),
+	('9787890123456', 'Sample Book 17', get_random_author_id(), 2019, 'Technology', 320, 'Greek'),
+	('9788901234567', 'Sample Book 18', get_random_author_id(), 2020, 'Business', 280, 'Hebrew'),
+	('9789012345678', 'Sample Book 19', get_random_author_id(), 2018, 'Finance', 400, 'Polish'),
+	('9780123456789', 'Sample Book 20', get_random_author_id(), 2022, 'Health', 300, 'Norwegian');
+END$$;
+
 CREATE OR REPLACE PROCEDURE populate_database()
 LANGUAGE plpgsql AS $$
 BEGIN
