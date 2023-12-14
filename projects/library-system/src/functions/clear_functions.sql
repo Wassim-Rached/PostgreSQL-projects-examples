3c3affafad5b4cac9e010ab699ef5adf5b4c28bb
wassim <wassimrached404@gmail.com>
2023-12-14 23:26:40 +0100

    Add clear functions to empty tables in the database

---
+++ projects/library-system/src/functions/clear_functions.sql
@@ -0,0 +1,69 @@
+-- empty the address table
+CREATE OR REPLACE PROCEDURE empty_addresses()
+LANGUAGE plpgsql AS $$
+BEGIN
+	TRUNCATE TABLE Address CASCADE;
+END$$;
+
+-- empty the persons table
+CREATE OR REPLACE PROCEDURE empty_persons()
+LANGUAGE plpgsql AS $$
+BEGIN
+	TRUNCATE TABLE Person CASCADE;
+END$$;
+
+-- empty the authors table
+CREATE OR REPLACE PROCEDURE empty_authors()
+LANGUAGE plpgsql AS $$
+BEGIN
+	TRUNCATE TABLE Author CASCADE;
+END$$;
+
+-- empty the librarySubscriptions table
+CREATE OR REPLACE PROCEDURE empty_librarySubscriptions()
+LANGUAGE plpgsql AS $$
+BEGIN
+	TRUNCATE TABLE LibrarySubscription CASCADE;
+END$$;
+
+-- empty the librarySubscriptionPayments table
+CREATE OR REPLACE PROCEDURE empty_librarySubscriptionPayments()
+LANGUAGE plpgsql AS $$
+BEGIN
+	TRUNCATE TABLE LibrarySubscriptionPayment CASCADE;
+END$$;
+
+-- empty the books table
+CREATE OR REPLACE PROCEDURE empty_books()
+LANGUAGE plpgsql AS $$
+BEGIN
+	TRUNCATE TABLE Book CASCADE;
+END$$;
+
+-- empty the bookLoans table
+CREATE OR REPLACE PROCEDURE empty_bookLoans()
+LANGUAGE plpgsql AS $$
+BEGIN
+	TRUNCATE TABLE BookLoan CASCADE;
+END$$;
+
+-- empty the mulcts table
+CREATE OR REPLACE PROCEDURE empty_mulcts()
+LANGUAGE plpgsql AS $$
+BEGIN
+	TRUNCATE TABLE Mulct CASCADE;
+END$$;
+
+-- empty the whole database
+CREATE OR REPLACE PROCEDURE empty_database()
+LANGUAGE plpgsql AS $$
+BEGIN
+	CALL empty_mulcts();
+	CALL empty_bookLoans();
+	CALL empty_books();
+	CALL empty_librarySubscriptionPayments();
+	CALL empty_librarySubscriptions();
+	CALL empty_authors();
+	CALL empty_persons();
+	CALL empty_addresses();
+END$$;
