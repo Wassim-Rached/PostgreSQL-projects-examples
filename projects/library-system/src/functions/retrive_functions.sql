21f6bad1e03de3f67fb0061393c803d6068d5f19
wassim <wassimrached404@gmail.com>
2023-12-15 13:27:34 +0100

    Add new functions for retrieving random IDs

--- projects/library-system/src/functions/retrive_functions.sql
+++ projects/library-system/src/functions/retrive_functions.sql
@@ -36,11 +36,61 @@ BEGIN
 	RETURN var_id;
 END$$;
 
--- get person by id
-CREATE OR REPLACE FUNCTION get_person_by_id(p_id UUID)
-RETURNS SETOF Person
+
+-- get a random id from the 'Person' table taht is not a library subscriber
+-- (main perpose is to be used in the 'populate_dummy_librarySubscriptions' procedure)
+CREATE OR REPLACE FUNCTION get_random_non_librarySubscriber_person_id()
+RETURNS UUID
 LANGUAGE plpgsql AS $$
+DECLARE 
+	var_id UUID;
 BEGIN
-	RETURN QUERY SELECT * FROM Person p1 WHERE p1.person_id = p_id;
+	SELECT person_id INTO var_id FROM Person WHERE person_id NOT IN (SELECT person_id FROM LibrarySubscription) ORDER BY random() LIMIT 1;
+	RETURN var_id;
 END$$;
 
+-- get a random id from the 'LibrarySubscription' table
+-- (main perpose is to be used in the 'populate_dummy_librarySubscriptionPayments' procedure)
+CREATE OR REPLACE FUNCTION get_random_librarySubscription_id()
+RETURNS UUID
+LANGUAGE plpgsql AS $$
+DECLARE 
+	var_id UUID;
+BEGIN
+	SELECT librarySubscription_id INTO var_id FROM LibrarySubscription ORDER BY random() LIMIT 1;
+	RETURN var_id;
+END$$;
+
+-- get a random book id from the 'Book' table
+-- (main perpose is to be used in the 'populate_dummy_BookLoan' procedure)
+CREATE OR REPLACE FUNCTION get_random_book_id()
+RETURNS UUID
+LANGUAGE plpgsql AS $$
+DECLARE 
+	var_id UUID;
+BEGIN
+	SELECT book_id INTO var_id FROM Book ORDER BY random() LIMIT 1;
+	RETURN var_id;
+END$$;
+
+-- get random person id
+CREATE OR REPLACE FUNCTION get_random_person_id()
+RETURNS UUID
+LANGUAGE plpgsql AS $$
+DECLARE 
+	var_id UUID;
+BEGIN
+	SELECT person_id INTO var_id FROM Person ORDER BY random() LIMIT 1;
+	RETURN var_id;
+END$$;
+
+-- get random 'MulctType' id
+CREATE OR REPLACE FUNCTION get_random_mulctType_id()
+RETURNS UUID
+LANGUAGE plpgsql AS $$
+DECLARE 
+	var_id UUID;
+BEGIN
+	SELECT mulctType_id INTO var_id FROM MulctType ORDER BY random() LIMIT 1;
+	RETURN var_id;
+END$$;
