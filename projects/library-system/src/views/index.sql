f73bae80bf1f63ac524dfb4d033fee7d9f255018
wassim <82286951+Wassim-Rached@users.noreply.github.com>
2023-12-17 23:13:34 +0100

    Update index.sql

--- projects/library-system/src/views/index.sql
+++ projects/library-system/src/views/index.sql
@@ -20,4 +20,4 @@ CREATE OR REPLACE VIEW librarySubscription_fullInfo AS
 		) AS subscription_expired
 	FROM
 		LibrarySubscription AS ls
-		INNER JOIN person_fullInfo AS p_fi USING(person_id);
+		INNER JOIN person_fullInfo USING(person_id);
